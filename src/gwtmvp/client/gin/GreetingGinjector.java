06c20ecbe4998a6f32a08ddacf1d33c3f4545483
Raidok <RKalbre@gmail.com>
2012-04-09 19:39:44 +0300

    missing annotation

--- src/gwtmvp/client/gin/GreetingGinjector.java
+++ src/gwtmvp/client/gin/GreetingGinjector.java
@@ -1,10 +1,13 @@
 package gwtmvp.client.gin;
 
 import gwtmvp.client.mvp.AppPresenter;
+import net.customware.gwt.dispatch.client.gin.StandardDispatchModule;
 import net.customware.gwt.presenter.client.place.PlaceManager;
 
+import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 
+@GinModules({ GreetingClientModule.class, StandardDispatchModule.class })
 public interface GreetingGinjector extends Ginjector {
 	AppPresenter getAppPresenter();
 	PlaceManager getPlaceManager();
