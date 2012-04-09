bf381cf9acb6a0c535d4290d9354b62b7506fe57
Raidok <RKalbre@gmail.com>
2012-04-09 13:49:56 +0300

    logging

--- src/gwtmvp/server/guice/GuiceServerModule.java
+++ src/gwtmvp/server/guice/GuiceServerModule.java
@@ -4,11 +4,16 @@ import gwtmvp.server.handler.SendGreetingHandler;
 import gwtmvp.shared.rpc.SendGreeting;
 import net.customware.gwt.dispatch.server.guice.ActionHandlerModule;
 
+import org.apache.commons.logging.Log;
+
+import com.google.inject.Singleton;
+
 public class GuiceServerModule extends ActionHandlerModule {
 
 	@Override
 	protected void configureHandlers() {
 		bindHandler(SendGreeting.class, SendGreetingHandler.class);
+		bind(Log.class).toProvider(LogProvider.class).in(Singleton.class);
 	}
 
 }
