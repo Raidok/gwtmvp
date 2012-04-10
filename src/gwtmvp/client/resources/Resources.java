4363e9bc0626ad9c6b8dd95267cae437e157de4e
Raidok <RKalbre@gmail.com>
2012-04-11 00:46:07 +0300

    moved style.css and made a ClientBundle class

---
+++ src/gwtmvp/client/resources/Resources.java
@@ -0,0 +1,11 @@
+package gwtmvp.client.resources;
+
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+public interface Resources extends ClientBundle {
+
+	@Source("style.css")
+	@CssResource.NotStrict
+	CssResource css();
+}
