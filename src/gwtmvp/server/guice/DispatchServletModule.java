ae7bf2c349da8f6c48c2e8805740343c6c55cc2d
Raidok <RKalbre@gmail.com>
2012-04-10 15:53:03 +0300

    correct servlet path

--- src/gwtmvp/server/guice/DispatchServletModule.java
+++ src/gwtmvp/server/guice/DispatchServletModule.java
@@ -2,13 +2,14 @@ package gwtmvp.server.guice;
 
 import com.google.inject.servlet.ServletModule;
 import com.gwtplatform.dispatch.server.guice.DispatchServiceImpl;
+import com.gwtplatform.dispatch.shared.ActionImpl;
 
 public class DispatchServletModule extends ServletModule {
 
 	@Override
 	protected void configureServlets() {
 		super.configureServlets();
-		serve("/gwtmvp/dispatch").with(DispatchServiceImpl.class);
+		serve("/" + ActionImpl.DEFAULT_SERVICE_NAME+ "*").with(DispatchServiceImpl.class);
 	}
 
 }
