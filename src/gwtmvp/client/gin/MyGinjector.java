7fe7fbe1bc24e7cec2471bb91e7539eb8f8ad14b
Raidok <RKalbre@gmail.com>
2012-04-17 19:33:13 +0300

    invalid getter

--- src/gwtmvp/client/gin/MyGinjector.java
+++ src/gwtmvp/client/gin/MyGinjector.java
@@ -9,7 +9,6 @@ import com.google.gwt.inject.client.AsyncProvider;
 import com.google.gwt.inject.client.GinModules;
 import com.google.gwt.inject.client.Ginjector;
 import com.google.gwt.resources.client.CssResource;
-import com.google.gwt.resources.client.ImageResource;
 import com.google.inject.Provider;
 import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.dispatch.client.gin.DispatchAsyncModule;
@@ -20,7 +19,6 @@ public interface MyGinjector extends Ginjector {
 	EventBus getEventBus();
 	PlaceManager getPlaceManager();
 	CssResource getCss();
-	ImageResource getLoadingImage();
 	Provider<LayoutPresenter> getLayoutPresenter();
 	Provider<HomePresenter> getHomePresenter();
 	AsyncProvider<AdminPresenter> getAdminPresenter();
